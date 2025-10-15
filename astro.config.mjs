93a46bbbe5dcddd525d51d603920c54c92cfc9f2
Dreykp <poturayvlad@gmail.com>
2025-10-15 20:54:56 +0300

    Test commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,36 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    site: 'https://dreykp.github.io',
+    base: '/awesome-open-source',
+    integrations: [
+        starlight({
+            title: 'Awesome open source',
+            defaultLocale: 'en',
+            locales: {
+                en: {label: 'English'},
+                uk: {label: 'Українська'}
+            },
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+            sidebar: [
+                // 👇 ОСЬ НОВИЙ БЛОК ДЛЯ ВАШОГО КАТАЛОГУ
+                {
+                    label: 'Каталог Додатків', // Назва секції в меню
+                    autogenerate: { directory: 'apps' }, // Автоматично генерувати меню з папки 'apps'
+                },
+                // ЦЕ ЗАЛИШКОВІ БЛОКИ, ЇХ МОЖНА ВИДАЛИТИ АБО ЗАЛИШИТИ
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
+    ],
 });
